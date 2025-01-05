a7e99ce5ff40bb7acb78420ba3349e702ee5d6ea
Deepak Dhiman <deepakdhimandd14@gmail.com>
2025-01-05 15:00:26 +0530

    new

--- tros frontend/src/components/Header.jsx
+++ tros frontend/src/components/Header.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <div className='header-main absolute z-10 top-0'>
-          <div className='container'>
-            <div className='header-inner'>
-              <div className='logo'>Hello</div>
+        <div className='header-main relative z-10 top-0'>
+          <div className='sticky top-0'>
+            <div className='container'>
+              <div className='header-inner grid grid-cols-12'>
+                <div className='logo col-span-2'>Hello</div>
+                <div className='menu col-span-8'>Hello</div>
+                <div className='header-btn col-span-2'>Hello</div>
+              </div>
             </div>
           </div>
         </div>
